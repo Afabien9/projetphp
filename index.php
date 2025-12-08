96ba43bbd7b6b87a2f002b2a8f8770e2e9f00c60
Achille <achille.coves13@gmail.com>
2025-12-08 12:08:06 +0100

    Stylisation du html

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bataille Naval</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 
 <body>
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
@@ -19,6 +20,175 @@
 
     <main>
 
+    <div class="container mt-5 ml-5">
+
+        <h1 id="message"> Grille adverse</h1>
+        <!-- Ligne des chiffres -->
+        <div class="row text-center">
+            <div class="col square"></div>
+            <div class="col border square">1</div>
+            <div class="col border square">2</div>
+            <div class="col border square">3</div>
+            <div class="col border square">4</div>
+            <div class="col border square">5</div>
+            <div class="col border square">6</div>
+            <div class="col border square">7</div>
+            <div class="col border square">8</div>
+            <div class="col border square">9</div>
+            <div class="col border square">10</div>
+        </div>
+
+
+        <!-- Ligne A -->
+        <div class="row text-center">
+            <div class="col border square">A</div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+        </div>
+
+        <!-- Ligne B -->
+        <div class="row text-center">
+            <div class="col border square">B</div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+        </div>
+
+        <!-- Ligne C -->
+        <div class="row text-center">
+            <div class="col border square">C</div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+        </div>
+
+        <!-- Ligne D -->
+        <div class="row text-center">
+            <div class="col border square">D</div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+        </div>
+
+        <!-- Ligne E -->
+        <div class="row text-center">
+            <div class="col border square">E</div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+        </div>
+
+        <!-- Ligne F -->
+        <div class="row text-center">
+            <div class="col border square">F</div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+        </div>
+
+        <!-- Ligne G -->
+        <div class="row text-center">
+            <div class="col border square">G</div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+        </div>
+
+        <!-- Ligne H -->
+        <div class="row text-center">
+            <div class="col border square">H</div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+        </div>
+
+        <!-- Ligne I -->
+        <div class="row text-center">
+            <div class="col border square">I</div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+        </div>
+
+        <!-- Ligne J -->
+        <div class="row text-center">
+            <div class="col border square">J</div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+            <div class="col border square"></div>
+        </div>
+    </div>
     </main>
 
     <footer>
